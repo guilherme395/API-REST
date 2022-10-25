24f17f87ff36b7c8e9f2e45f926a28107dd9441b
Guilherme Lima <guilhermell@live.com>
2022-10-25 16:45:20 -0300

    ....

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require("./app");
 const server = http.createServer(app);
-const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 server.listen(port, host, function () {
     console.log("Server started.......");
