dcc7cf850b0799b033e1478b0a26dbe120aa3de5
Guilherme Lima <guilhermell@live.com>
2022-11-03 15:28:53 -0300

    .....

--- src/app.js
+++ src/app.js
@@ -21,11 +21,7 @@ app.use("/api/v1/user/register", UserRouter);
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Header",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-
+    res.header("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).send({});
