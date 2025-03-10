a92814e8ee11d3cf78ba30ca3c9c4947f83652ba
Guilherme Lima Leite <guilhermelimaleite06@gmail.com>
2025-03-10 11:28:22 -0300

    Refatorado gerenciamento de rotas de usuários para utilizar método chaining e melhorar a legibilidade

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,20 @@
 import express from "express";
-const router = express.Router();
 import userController from "./controllers/userController.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
 
-router.post("/users", userController.createUser);
-router.put(
-	"/users/:id",
-	authMiddleware.validateToken,
-	userController.updateUser
-);
-router.delete(
-	"/users/:id",
-	authMiddleware.validateToken,
-	userController.deleteUser
-);
-router.get("/users", authMiddleware.validateToken, userController.listUsers);
-router.get("/users/:id", authMiddleware.validateToken, userController.getUser);
+const router = express.Router();
+
+router
+	.route("/users")
+	.post(userController.createUser)
+	.get(authMiddleware.validateToken, userController.listUsers);
+
+router
+	.route("/users/:id")
+	.get(authMiddleware.validateToken, userController.getUser)
+	.put(authMiddleware.validateToken, userController.updateUser)
+	.delete(authMiddleware.validateToken, userController.deleteUser);
+
 router.post("/users/auth", userController.authenticateUser);
 
 export default router;
