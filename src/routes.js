62fbc5770430875824933db5f46e06152a990197
Guilherme Lima Leite <guilhermelimaleite06@gmail.com>
2025-03-10 10:23:48 -0300

    Adicionado middleware de autenticação para validação de token em rotas de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import express from "express";
 const router = express.Router();
 import userController from "./controllers/userController.js";
+import authMiddleware from "./middlewares/authMiddleware.js";
 
 router.post("/users", userController.createUser);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
-router.get("/users", userController.listUsers);
-router.get("/users/:id", userController.getUser);
+router.put(
+	"/users/:id",
+	authMiddleware.validateToken,
+	userController.updateUser
+);
+router.delete(
+	"/users/:id",
+	authMiddleware.validateToken,
+	userController.deleteUser
+);
+router.get("/users", authMiddleware.validateToken, userController.listUsers);
+router.get("/users/:id", authMiddleware.validateToken, userController.getUser);
 router.post("/users/auth", userController.authenticateUser);
 
 export default router;
